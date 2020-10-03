88e4031de70752f202f6c08542f73b26b199aab2
tsu019 <marisachanth@gmail.com>
2020-10-03 13:24:11 -0700

    fix folder structure

---
+++ src/components/pages/Landing/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Hero from '../../templates/Hero';
+import Overview from '../../templates/Overview';
+import Features from '../../templates/Features';
+
+const Landing = () => {
+  return(
+    <div>
+      <Hero />
+      <Overview />
+      <Features />
+    </div>
+  )
+}
+
+export default Landing;
